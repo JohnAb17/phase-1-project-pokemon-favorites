8846979ff710acf713dc7c5cdf7a09b710423adf
John Medina <john9713medina@hotmail.com>
2023-05-17 21:56:07 -0700

    updated the fetch URL

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 console.log("index.js is working!")
 
-fetch("http://localhost:3000/pokemon").then(response => response.json()).then(json => console.log(json))
+fetch("https://johnab17.github.io/phase-1-project-pokemon-favorites/")
+.then((response) => response.json())
+.then((pokemons) => {
+    const container = document.querySelector("ul#pokemonList");
+    pokemons.forEach(pokemon => {
+        //name , description, type
+        //create element to hold the name 
+        //create element to hold description, p in html
+        //create element to holf type
+        //create a list item (li) to hold the name and description
+        //append elements to list item
+        //append list item to container
+        
+
+    })
+
+});
