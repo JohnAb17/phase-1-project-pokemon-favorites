6af147fa3554dc34c1b73f30a873e54c8a069a9c
John Medina <john9713medina@hotmail.com>
2023-05-18 06:27:49 -0700

    changes according to a working example

--- index.js
+++ index.js
@@ -1,80 +1,137 @@
-console.log("index.js is working!")
+// console.log("index.js is working!")
+
+
+// const searchForm = document.getElementById('search-form');
+// const searchInput = document.getElementById('search-input');
+// const pokemonCard = document.getElementById('pokemonCard');
+// const randomButton = document.getElementById('random-button');
+
+
+// function handleSearch(event, pokemons) {
+//     event.preventDefault();          
+//     const searchQuery = searchInput.value.toLowerCase();
+//     const findPokemons = pokemons.find(pokemon =>
+//       pokemon.name.toLowerCase().includes(searchQuery)
+//     );
+          
+//     displayPokemon(findPokemons);
+// }
+
+// const displayPokemon = pokemon => {
+//     pokemonCard.textContent = '';
+
+//     if (pokemon) {
+//             const card = document.createElement('div');
+//             card.classList.add('pokemon-card');
+
+//             const image = document.createElement('img');
+//             image.classList.add('pokemon-image');
+//             image.src = pokemon.image;
+//             image.alt = pokemon.name;
+
+//             const name = document.createElement('h2');
+//             name.classList.add('pokemon-name');
+//             name.textContent = pokemon.name;
+
+//             const type = document.createElement('p');
+//             type.classList.add('pokemon-type');
+//             type.textContent = `Type: ${pokemon.type}`;
+
+//             const description = document.createElement('p');
+//             description.classList.add('pokemon-description');
+//             description.textContent = pokemon.description;
+
+//             card.appendChild(name);
+//             card.appendChild(image);
+//             card.appendChild(type);
+//             card.appendChild(description);
+
+//             pokemonCard.appendChild(card);
+
+//     } else {
+//         const message = document.createElement('p');
+//         message.textContent = 'No Pokemon Found, Remember only Eeveelutions here!';
+//         pokemonCard.appendChild(message);
+//     }
+// }
+
+// function getRandomPokemon(pokemons) {
+//     const randomIndex = Math.floor(Math.random() * pokemons.length);
+//     return pokemons[randomIndex];
+// }
+
+// fetch("db.json")
+//   .then(response => response.json())
+//   .then(data => {
+//     pokemons = data.pokemons;
+
+//     searchForm.addEventListener('submit', handleSearch);
+
+//     searchInput.addEventListener('keydown', event => {
+//       if (event.key === 'Enter') {
+//         handleSearch(event);
+//       }
+//     });
+
+//     randomButton.addEventListener('click', () => {
+//       const randomPokemon = getRandomPokemon(pokemons);
+//       displayPokemon(randomPokemon);
+//     });
+// });   
 
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
-const pokemonCard = document.getElementById('pokemonCard');
-const randomButton = document.getElementById('random-button');
-let pokemons = [];
+const pokemonList = document.getElementById('pokemonList');
 
-function handleSearch(event, pokemons) {
-    event.preventDefault();          
-    const searchQuery = searchInput.value.toLowerCase();
-    const findPokemons = pokemons.find(pokemon =>
-      pokemon.name.toLowerCase().includes(searchQuery)
-    );
-          
-    displayPokemon(findPokemons);
-}
-
-const displayPokemon = pokemon => {
-    pokemonCard.textContent = '';
-
-    if (pokemon) {
-            const card = document.createElement('div');
-            card.classList.add('pokemon-card');
-
-            const image = document.createElement('img');
-            image.classList.add('pokemon-image');
-            image.src = pokemon.image;
-            image.alt = pokemon.name;
-
-            const name = document.createElement('h2');
-            name.classList.add('pokemon-name');
-            name.textContent = pokemon.name;
-
-            const type = document.createElement('p');
-            type.classList.add('pokemon-type');
-            type.textContent = `Type: ${pokemon.type}`;
-
-            const description = document.createElement('p');
-            description.classList.add('pokemon-description');
-            description.textContent = pokemon.description;
-
-            card.appendChild(name);
-            card.appendChild(image);
-            card.appendChild(type);
-            card.appendChild(description);
-
-            pokemonCard.appendChild(card);
-
-    } else {
-        const message = document.createElement('p');
-        message.textContent = 'No Pokemon Found, Remember only Eeveelutions here!';
-        pokemonCard.appendChild(message);
-    }
-}
-
-function getRandomPokemon(pokemons) {
-    const randomIndex = Math.floor(Math.random() * pokemons.length);
-    return pokemons[randomIndex];
-}
-
-fetch("db.json")
+fetch('db.json')
   .then(response => response.json())
   .then(data => {
-    pokemons = data.pokemons;
+    const pokemons = data.pokemons;
 
-    searchForm.addEventListener('submit', handleSearch);
+    searchForm.addEventListener('submit', function(event) {
+      event.preventDefault();
 
-    searchInput.addEventListener('keydown', event => {
-      if (event.key === 'Enter') {
-        handleSearch(event);
-      }
+      const searchQuery = searchInput.value.toLowerCase();
+
+      const filteredPokemons = pokemons.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(searchQuery)
+      );
+
+      displayPokemons(filteredPokemons);
     });
 
-    randomButton.addEventListener('click', () => {
-      const randomPokemon = getRandomPokemon(pokemons);
-      displayPokemon(randomPokemon);
-    });
-  });   
+    displayPokemons(pokemons);
+  });
 
+function displayPokemons(pokemons) {
+  pokemonList.innerHTML = '';
+
+  pokemons.forEach(pokemon => {
+    const card = document.createElement('div');
+    card.classList.add('pokemon-card');
+
+    const image = document.createElement('img');
+    image.classList.add('pokemon-image');
+    image.src = pokemon.image;
+    image.alt = pokemon.name;
+
+    const name = document.createElement('h2');
+    name.classList.add('pokemon-name');
+    name.textContent = pokemon.name;
+
+    const type = document.createElement('p');
+    type.classList.add('pokemon-type');
+    type.textContent = `Type: ${pokemon.type}`;
+
+    const description = document.createElement('p');
+    description.classList.add('pokemon-description');
+    description.textContent = pokemon.description;
+
+    card.appendChild(image);
+    card.appendChild(name);
+    card.appendChild(type);
+    card.appendChild(description);
+
+    pokemonList.appendChild(card);
+  });
+}
