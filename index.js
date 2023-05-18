5761fccd629cabfbb4bd354a02c30eda2a908631
John Medina <john9713medina@hotmail.com>
2023-05-17 22:13:53 -0700

    created elements to fit json objects

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 console.log("index.js is working!")
 
 fetch("https://johnab17.github.io/phase-1-project-pokemon-favorites/")
-.then((response) => response.json())
-.then((pokemons) => {
-    const container = document.querySelector("ul#pokemonList");
-    pokemons.forEach(pokemon => {
-        //name , description, type
+.then(response => response.json())
+.then(pokemons => {
+    console.log(pokemons);
+
+    pokemons.data.forEach(item => {
+        console.log(item.id);
+        console.log(item.name);
+        console.log(item.image);
+        console.log(item.type);
+        console.log(item.description);
+        
+        //create elemnt to holf id
         //create element to hold the name 
-        //create element to hold description, p in html
+        //create element to hold img
         //create element to holf type
+        //create element to hold description, p in html
+
         //create a list item (li) to hold the name and description
         //append elements to list item
         //append list item to container
         
-
+        
     })
 
 });
+
